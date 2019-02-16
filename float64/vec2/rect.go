a7d18ec4752429ebe314581f96f2e5fa2fc47522
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-02-16 10:07:27 -0500

    Fix Intersects

--- float64/vec2/rect.go
+++ float64/vec2/rect.go
@@ -48,10 +48,13 @@ func (rect *Rect) Area() float64 {
 }
 
 func (rect *Rect) Intersects(other *Rect) bool {
-	return other.Max[0] >= rect.Min[0] &&
-		other.Min[0] <= rect.Max[0] &&
-		other.Max[1] >= rect.Min[0] &&
-		other.Min[1] <= rect.Max[1]
+	if other.Min[0] > rect.Max[0] || rect.Min[0] > other.Max[0] {
+		return false
+	}
+	if other.Min[1] > rect.Max[1] || rect.Min[1] > other.Max[1] {
+		return false
+	}
+	return true
 }
 
 // func Intersect(a, b *Rect) Rect {
--- float64/vec2/rect_test.go
+++ float64/vec2/rect_test.go
@@ -27,3 +27,23 @@ func TestJoined(t *testing.T) {
 		t.Errorf("failed Joined: got %v, want %v", got, want)
 	}
 }
+
+func TestIntersects(t *testing.T) {
+	a := Rect{T{-0.07499999999999983, 1.3818309886183793}, T{15.031830988618381, 1.5318309886183792}}
+	b := Rect{T{2.306134154342999, -11.530670771714988}, T{25.367475697772974, 11.530670771714988}}
+	if got, want := a.Intersects(&b), true; got != want {
+		t.Errorf("failed Intersects: got %v, want %v", got, want)
+	}
+	if got, want := b.Intersects(&a), true; got != want {
+		t.Errorf("failed Intersects: got %v, want %v", got, want)
+	}
+
+	a = Rect{T{0, 0}, T{1, 1}}
+	b = Rect{T{0, 2}, T{0, 3}}
+	if got, want := a.Intersects(&b), false; got != want {
+		t.Errorf("failed Intersects: got %v, want %v", got, want)
+	}
+	if got, want := b.Intersects(&a), false; got != want {
+		t.Errorf("failed Intersects: got %v, want %v", got, want)
+	}
+}
