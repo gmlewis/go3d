fc1b5a36db1f54ff910eb58dda190a8ca7925589
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-02-16 00:03:56 -0500

    Add Intersects to float/vec2

--- float64/vec2/rect.go
+++ float64/vec2/rect.go
@@ -47,9 +47,12 @@ func (rect *Rect) Area() float64 {
 	return (rect.Max[0] - rect.Min[0]) * (rect.Max[1] - rect.Min[1])
 }
 
-// func (rect *Rect) Intersects(other *Rect) bool {
-// 	panic("not implemented")
-// }
+func (rect *Rect) Intersects(other *Rect) bool {
+	return other.Max[0] >= rect.Min[0] &&
+		other.Min[0] <= rect.Max[0] &&
+		other.Max[1] >= rect.Min[0] &&
+		other.Min[1] <= rect.Max[1]
+}
 
 // func Intersect(a, b *Rect) Rect {
 // 	panic("not implemented")
